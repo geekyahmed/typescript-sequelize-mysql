d21926d5a91d7b97a78a80381c99415c3b177132
Ahmed Bankole <thegeekyahmed@gmail.com>
2022-11-19 12:00:22 +0100

    new changes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,1 @@
-import express, { Router } from 'express'
-
-const router = express.Router()
-
-interface Route {
-    path: string
-    route: Router
-}
-
-const routes: Route[] = {
-
-}
-
-export { routes }
+export { TodoRoute } from './todo.route'
