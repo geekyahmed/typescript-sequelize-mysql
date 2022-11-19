96ff00911a79d7bc424e17006b618000b47849f4
Ahmed Bankole <thegeekyahmed@gmail.com>
2022-11-19 12:14:11 +0100

    finish up server

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,68 @@ import express, { Request, Response, NextFunction } from 'express'
 import { json, urlencoded } from 'body-parser'
 
 import { connection } from './config'
+import { Route } from './interfaces'
 
-const app = express()
+class App {
+    public app: express.Application
+    public port: string | number
+    public env: string
 
-const PORT = 3000
+    constructor(routes: Route) {
+        this.app = express()
+        this.port = process.env.PORT || 3000
+        this.env = process.env.NODE_ENV || 'development'
 
-app.use(json())
+        this.connectDatabase()
+        this.initializeMiddlewares()
+        this.initializeRoutes(routes)
+    }
 
-app.use(urlencoded({
-    extended: true
-}))
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`=================================`);
+            console.log(`======= CURRENT ENV: ${this.env} =======`);
+            console.log(`🚀 App listening on the port ${this.port} 🚀`);
+            console.log(`=================================`);
+        });
+    }
 
-// Error checkxing middleware
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    return res.status(500).json({
-        message: err.message
-    })
-})
+    public getServer() {
+        return this.app;
+    }
 
-connection
-    .sync()
-    .then(() => {
-        console.log('Database Is Running Successfully 🚀🚀')
-    })
-    .catch((err: Error) => {
-        console.error(err)
-    })
+    private connectDatabase() {
+        connection
+            .sync()
+            .then(() => {
+                console.log(`=================================`);
+                console.log(`🚀 Connected to ${process.env.DB_NAME} database successfully 🚀`);
+                console.log(`=================================`);
+            })
+            .catch((err: Error) => {
+                console.error(err)
+            })
+    }
 
-app.listen(PORT, () => {
-    console.log(`Server Running At 127.0.0.1:${PORT} 🚀🚀`)
-})
+    private initializeMiddlewares() {
+        this.app.use(json())
+
+        this.app.use(urlencoded({
+            extended: true
+        }))
+
+        this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+            return res.status(500).json({
+                message: err.message
+            })
+        })
+    }
+
+    private initializeRoutes(routes: Route[]) {
+        routes.forEach(route => {
+            this.app.use('/', route.router);
+        });
+    }
+}
+
+export { App }
